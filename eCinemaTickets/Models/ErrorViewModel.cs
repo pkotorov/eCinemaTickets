1ab1d8ee82259a2f7057a6b2178df6a43cd9ba30
Petar Kotorov <petar.kotorov@yahoo.de>
2021-11-26 18:19:19 +0100

    Add project files.

---
+++ eCinemaTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eCinemaTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
